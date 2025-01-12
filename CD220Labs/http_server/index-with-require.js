e45ba2a35991bef00992f8dbdf82fbf486391ef5
JayJay247in <cj193532@gmail.com>
2025-01-12 11:30:09 -0500

    Node.js used for the 1st time

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,22 +1,32 @@
- // Import the HTTP module
 const http = require('http');
-
-// Import the 'today' module
+const url = require('url');
 const today = require('./today');
 
-// Define the request listener function
-const requestListener = function (req, res) {
-    res.writeHead(200); // Set the status code to 200 (OK)
-    // Send the response with the current date from the 'today' module
-    res.end(`Hello, World! The date today is ${today.getDate()}`);
-};
+const server = http.createServer(function(req, res) {
+  const parsedUrl = url.parse(req.url, true);
+  const pathname = parsedUrl.pathname;
+    let message = 'Hello World!';
 
-// Define the port number
+  if (pathname === '/time') {
+        const dateVal = today.getDate();
+        if (dateVal.getHours() > 6 && dateVal.getHours() < 12) {
+        message = "Good Morning";
+        } else if (dateVal.getHours() >= 12 && dateVal.getHours() < 18) {
+            message = "Good Afternoon";
+        } else {
+        message = "Good Evening";
+        }
+  }
+   else if(pathname !== '/') {
+        message = "404 Not Found";
+    }
+
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(message);
+});
+
 const port = 8080;
-
-// Create an HTTP server using the request listener function
-const server = http.createServer(requestListener);
-
-// Start the server and listen on the specified port
 server.listen(port);
-console.log('Server listening on port: ' + port);
+
+console.log(`Server listening on port: ${port}`);
--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,6 +1,5 @@
- // Export a function named 'getDate' from the module
+// Export a function named 'getDate' from the module
 module.exports.getDate = function getDate() {
     // Get the current date and time in the timezone "Australia/Brisbane"
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return aestTime; // Return the formatted date and time
+    return new Date(new Date().toLocaleString("en-US", { timeZone: "Australia/Brisbane" })); // Return the Date Object
 };
